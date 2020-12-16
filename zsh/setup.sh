813b081687256bd4ee8fabf5376b7f27c5fc4a01
B3ale <b3ale@localhost.localdomain>
2020-12-16 06:49:01 -0500

    fix ohmyzsh repo

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "Install oh-my-zsh ..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 cp -v alias.zsh ~/.alias
