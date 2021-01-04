d99d2f20b7719975f26458b8950be34f7bd05d44
B3ale <b3ale@localhost.localdomain>
2021-01-04 04:16:06 -0500

    fix zsh plugins

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -5,6 +5,9 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 cp -v alias.zsh ~/.alias
 cp -v zshrc ~/.zshrc
 
